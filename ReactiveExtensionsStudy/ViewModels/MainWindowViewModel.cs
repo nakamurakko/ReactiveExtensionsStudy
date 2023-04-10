3d01962353f01d5ffdab7803f1a33140e3efae98
nakamurakko <28667444+nakamurakko@users.noreply.github.com>
2023-04-10 14:37:28 +0900

    挨拶のサンプルを追加。

--- ReactiveExtensionsStudy/ViewModels/MainWindowViewModel.cs
+++ ReactiveExtensionsStudy/ViewModels/MainWindowViewModel.cs
@@ -1,7 +1,9 @@
 ﻿using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reactive.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -11,5 +13,29 @@ namespace ReactiveExtensionsStudy.ViewModels
     {
         [ObservableProperty]
         private string _title = "ReactiveExtensionsStudy";
+
+        [ObservableProperty]
+        public string _greeting = "";
+
+        [ObservableProperty]
+        public string _greetingTo = "Everyone";
+
+        [RelayCommand]
+        public void ShowGreeting()
+        {
+            Observable.Return($@"Hello {GreetingTo}.")
+                .Delay(TimeSpan.FromSeconds(3))
+                .Subscribe(x =>
+                {
+                    Greeting = "";
+
+                    if (!string.IsNullOrEmpty(x))
+                    {
+                        Greeting = x;
+                    }
+                });
+
+            Greeting = "Greetings!";
+        }
     }
 }
