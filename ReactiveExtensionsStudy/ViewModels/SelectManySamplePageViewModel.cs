bc906790628335587097cd5331d192ed539a0202
nakamurakko <28667444+nakamurakko@users.noreply.github.com>
2023-04-10 18:58:13 +0900

    SelectMany のサンプルを追加。

---
+++ ReactiveExtensionsStudy/ViewModels/SelectManySamplePageViewModel.cs
@@ -0,0 +1,35 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Reactive.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ReactiveExtensionsStudy.ViewModels
+{
+    internal partial class SelectManySamplePageViewModel : ObservableObject
+    {
+        [ObservableProperty]
+        private string _title = "SelectMany";
+
+        [ObservableProperty]
+        private ObservableCollection<string> _users = new ObservableCollection<string>();
+
+        [RelayCommand]
+        public void Execute()
+        {
+            Users.Clear();
+
+            Observable.Return(new List<string>() { "山田太郎", "田中次郎", "中島三郎" })
+                .SelectMany(x => x)
+                .Select(x => x + "さん")
+                .Subscribe(x =>
+                {
+                    Users.Add(x);
+                });
+        }
+    }
+}
