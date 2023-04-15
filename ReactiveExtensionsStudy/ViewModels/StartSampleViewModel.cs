93bc62e07a125a73268fe11a1cd6d568ac930d3e
nakamurakko <28667444+nakamurakko@users.noreply.github.com>
2023-04-15 18:34:19 +0900

    Start のサンプルを追加。

---
+++ ReactiveExtensionsStudy/ViewModels/StartSampleViewModel.cs
@@ -0,0 +1,51 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reactive.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ReactiveExtensionsStudy.ViewModels
+{
+    internal partial class StartSampleViewModel : ObservableObject
+    {
+        [ObservableProperty]
+        public string _title = "StartSample";
+
+        [ObservableProperty]
+        public string _message = "";
+
+        [RelayCommand]
+        public void HelloWorld()
+        {
+            Message = "";
+
+            Observable.Start(() =>
+            {
+                return "Hello world.";
+            })
+                .Subscribe(x =>
+                {
+                    Message = x;
+                });
+        }
+
+        [RelayCommand]
+        public void HelloWorldWithDelay()
+        {
+            Message = "";
+
+            Observable.Start(() =>
+            {
+                return "Hello world with Delay.";
+            })
+                .Delay(TimeSpan.FromSeconds(3))
+                .Subscribe(x =>
+                {
+                    Message = x;
+                });
+        }
+    }
+}
